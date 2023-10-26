c988986f85ac0823bfa550b728eea22dacdb989c
ArhanWR <bintang135.bp@gmail.com>
2023-10-26 20:14:37 +0700

    Exercise 2

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,27 @@ void main() {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Icon(Icons.account_circle, size: 50),
-        ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
           children: [
-            Text(
-              'Flutter McFlutter',
-              style: Theme.of(context).textTheme.headline6,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.account_circle, size: 50),
             ),
-            const Text('Experienced App Developer'),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Flutter McFlutter',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Text('Experienced App Developer'),
+              ],
+            ),
           ],
         ),
         const SizedBox(height: 8),
