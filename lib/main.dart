c470634016000603cbe55f2061377c54dab83d2e
ArhanWR <bintang135.bp@gmail.com>
2023-10-26 19:47:44 +0700

    Aligning text

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,45 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: const [
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Futura',
+            color: Colors.blue,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 50,
+            fontFamily: 'Futura',
+            color: Colors.green,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 40,
+            fontFamily: 'Futura',
+            color: Colors.red,
+          ),
+        ),
+      ],
       //mainAxisSize: MainAxisSize.max,
       //mainAxisAlignment: MainAxisAlignment.spaceAround,
       //crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        BlueBox(),
-        const Spacer(flex: 1),
-        BlueBox(),
-        const Spacer(flex: 1),
-        BlueBox(),
+      //children: [
+        //BlueBox(),
+        //const Spacer(flex: 1),
+        //BlueBox(),
+        //const Spacer(flex: 1),
+        //BlueBox(),
         //const SizedBox(width: 25,),
-      ],
+      //],
       //children: [
       //  BlueBox(),
       //  Flexible(
