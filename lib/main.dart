89bfb33aee77a25a94d3e1455d9ab9c9762f1b48
ArhanWR <bintang135.bp@gmail.com>
2023-10-26 20:05:08 +0700

    Exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,64 +14,22 @@ void main() {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.network('https://raw.githubusercontent.com/flutter/website/main/examples/layout/sizing/images/pic3.jpg'),
+        Text(
+          'Flutter McFlutter',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        Text(
+          'Experienced App Developer',
+          style: TextStyle(
+            fontSize: 16, // Set the font size as needed
+            fontWeight: FontWeight.normal, // Set the font weight as needed
+          ),
+        ),
       ],
-
-
-      //mainAxisSize: MainAxisSize.max,
-      //mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //crossAxisAlignment: CrossAxisAlignment.center,
-      //children: [
-        //BlueBox(),
-        //const Spacer(flex: 1),
-        //BlueBox(),
-        //const Spacer(flex: 1),
-        //BlueBox(),
-        //const SizedBox(width: 25,),
-      //],
-      //children: [
-      //  BlueBox(),
-      //  Flexible(
-      //    fit: FlexFit.tight,
-      //    flex: 1,
-      //    child: BlueBox(),
-      //  ),
-      //  Flexible(
-      //    fit: FlexFit.tight,
-      //    flex: 1,
-      //    child: BlueBox(),
-      //  ),
-      //],
-    );
-  }
-}
-
-class BlueBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(),
-      ),
-    );
-  }
-}
-class BiggerBlueBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        border: Border.all(),
-      ),
     );
   }
 }
